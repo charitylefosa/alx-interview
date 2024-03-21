99d247f144fde33b6edb2f84ae5e929ce20b97d3
charitylefosa <charitylefosa@gmail.com>
2024-03-21 12:21:44 -0700

    main edit

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,32 +1,19 @@
 #!/usr/bin/python3
-"""
-Minimum Operations
-"""
+"""Minimum Operations"""
 
 
 def minOperations(n):
-    """
-    Calculates the fewest number of operations needed to result in exactly
-    n H characters in the file.
-    """
+    """Calculates fewest number of operations needed to result in exactly 
+    n H characters in the file."""
     if n <= 1:
         return 0
-
     operations = 0
     i = 2
-    while i * i <= n:
-        while n % i == 0:
+    while i <= n:
+        if n % i == 0:
             operations += i
             n /= i
-        i += 1
-    if n > 1:
-        operations += n
-    return int(operations)
+        else:
+            i += 1
+    return operations
 
-
-if __name__ == "__main__":
-    n = 4
-    print("Min number of operations to reach {} char: {}".format(n, minOperations(n)))
-
-    n = 12
-    print("Min number of operations to reach {} char: {}".format(n, minOperations(n)))
